5172f8c2c32c9dc349109497b3d6e9e7a1ed786e
Manohar Lotlapalli <manoharlotlapalli@Manohars-Laptop.local>
2024-12-11 18:53:04 +0530

    aadded Math methods

--- GIT/gitcommands.md
+++ GIT/gitcommands.md
@@ -1,1 +1,2 @@
 $ git init   // Intilize local git Reposotary.
+$ 
---
+++ JavaScript/01_variables-data-types/06_math-methods/script.js
@@ -0,0 +1,38 @@
+let x;
+
+// Math is Pre-definded JS methods (Math).
+
+// Square Root.
+x = Math.sqrt(9);
+
+// Absoulte Value
+x = Math.abs(-5);
+
+// Round.
+x = Math.round(6.2);
+
+// Round Up
+x = Math.ceil(6.2);
+
+// Round Down.
+x = Math.floor(4.9);
+
+// Exponenet. 2 ** 3;
+x = Math.pow(10,3);
+
+// Minum Number 1,2,3
+x = Math.min(10,28,31);
+
+// Max Number.
+x = Math.max(10,28,34);
+
+// Getting a randon number/decimal between 0 and 1 , 1 to 80,0000 (20,000)
+x = Math.random();
+
+// Get a Random number b/w 1 and 100.
+//x = Math.floor(Math.random() * 100 + 1);
+
+x = Math.round(Math.random() * 100);
+
+
+console.log(x);
